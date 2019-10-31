c0756c9a81ea000568cc4c7a9d3ef4d4763008c5
user <user@DESKTOP-3DNUHQ9>
2019-10-31 14:07:26 +0900

    123

--- Project4/소스.cpp
+++ Project4/소스.cpp
@@ -1,23 +1,51 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-	int n;
+	int a;
 
-	printf("정수 입력: ");
-	scanf("%d", &n);
+	printf("입력: ");
 
-	if (n % 2)
-		printf("홀수");
-	else
-		printf("짝수");
+	scanf("%d", &a);
 
-	printf("입니다\n");
+	if (a >= 360)
+	{
+		printf("잘못입력하였습니다.");
 
-	(n % 2) ? printf("홀수") : printf("짝수");
-	printf("입니다.\n");
+	}
+	else if (a > 0 && a < 90)
+	{
+		printf("1사분면"); 
+	}
+	else if (a > 90 && a < 180)
+	{
+		printf("2사분면");
 
-	return 0;
-	
+	}
+	else if (a > 180 && a < 270)
+	{
+		printf("3사분면");
+	}
+	else if (a > 270 && a < 360)
+	{
+		printf("4사분면");
+	}
+	else if (a == 0)
+	{
+		printf("양의 x축");
+	}
+	else if (a == 90)
+	{
+		printf("양의 Y축");
+	}
+	else if (a == 180)
+	{
+		printf("음의 x축");
+	}
+	else if (a == 270)
+	{
+		printf("음의 Y축");
+	}
+
 }
