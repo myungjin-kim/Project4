e26b643818a1e225401921bd87be321677f6e4d8
user <user@DESKTOP-3DNUHQ9>
2019-10-31 13:33:36 +0900

    6장 1번

---
+++ Project4/소스.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello, World\n");
+
+	int a = 1;
+	int b = 2;
+	int c = a + b;
+
+	printf("%d와 %d를 더하면 %d 입니다.\n", a, b, c);
+}
